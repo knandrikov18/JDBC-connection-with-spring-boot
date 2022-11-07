be8ea06c915d6d3d66644711858f1ef8f344745b
KNAndrikov18 <KNAndrikov18@codingburgas.bg>
2022-11-07 13:59:14 +0200

    Executing the querry

---
+++ src/main/java/com/example/connection_jdbc/Player.java
@@ -0,0 +1,11 @@
+package com.example.connection_jdbc;
+
+import java.util.Date;
+
+public class Player {
+    private int id;
+    private String name;
+    private String nationality;
+    private Date birthDate;
+    private int titles;
+}
