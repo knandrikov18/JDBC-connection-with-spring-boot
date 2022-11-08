9b4a90b8e891f0de517f2e98d19ccdabaf84575e
KNAndrikov18 <KNAndrikov18@codingburgas.bg>
2022-11-08 14:56:40 +0200

    Completing second pdf

--- src/main/java/com/example/connection_jdbc/PlayerDao.java
+++ src/main/java/com/example/connection_jdbc/PlayerDao.java
@@ -17,4 +17,9 @@ public class PlayerDao {
         String sql = "SELECT * FROM PLAYER";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Player>(Player.class));
     }
+    public Player getPlayerById(int id){
+        String sql = "SELECT * FROM PLAYER WHERE ID = ?";
+        return jdbcTemplate.queryForObject(sql,new BeanPropertyRowMapper<Player>(Player.class), new Object[] {id});
+    }
+
 }
